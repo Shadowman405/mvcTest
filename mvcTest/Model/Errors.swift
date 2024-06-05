98879d4106a5ca6f10ae54895d4a322b555925d1
Shadowman405 <maximus405@gmail.com>
2024-06-05 14:48:42 +0300

    Updated errors enum

--- mvcTest/Model/Errors.swift
+++ mvcTest/Model/Errors.swift
@@ -9,13 +9,16 @@ import Foundation
 
 enum DataErrors {
     case notAJSON
+    case cantParseJSON
 }
 
 extension DataErrors: LocalizedError {
     var errorDescription: String? {
         switch self {
         case .notAJSON:
-            return "Cant parse data from input - its not a JSON"
+            return "Cant parse data from input - Its not a JSON file."
+        case .cantParseJSON:
+            return "Cant parse JSON - You passed JSON with wrong format."
         }
     }
 }
